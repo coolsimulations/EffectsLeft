4bd0674295e8759f349fa69224b2078e66457615
coolsim <coolsimulations@gmail.com>
2022-06-10 11:36:18 +1000

    Update to 1.19

--- src/main/java/net/coolsimulations/EffectsLeft/mixin/ClientPacketListenerMixin.java
+++ src/main/java/net/coolsimulations/EffectsLeft/mixin/ClientPacketListenerMixin.java
@@ -7,12 +7,11 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.coolsimulations.EffectsLeft.EffectsLeftUpdateHandler;
-import net.minecraft.Util;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientPacketListener;
 import net.minecraft.network.chat.ClickEvent;
+import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.HoverEvent;
-import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.network.protocol.game.ClientboundLoginPacket;
 
 @Mixin(ClientPacketListener.class)
@@ -25,8 +24,8 @@ public class ClientPacketListenerMixin {
 	public void handleLogin(ClientboundLoginPacket packet, CallbackInfo info) {
 
 		if(EffectsLeftUpdateHandler.isOld == true) {
-			this.minecraft.player.sendMessage(EffectsLeftUpdateHandler.updateInfo.setStyle(EffectsLeftUpdateHandler.updateInfo.getStyle().withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TranslatableComponent("el.update.display2"))).withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://curseforge.com/minecraft/mc-mods/effectsleft-fabric"))), Util.NIL_UUID);
-			this.minecraft.player.sendMessage(EffectsLeftUpdateHandler.updateVersionInfo.setStyle(EffectsLeftUpdateHandler.updateVersionInfo.getStyle().withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TranslatableComponent("el.update.display2"))).withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://curseforge.com/minecraft/mc-mods/effectsleft-fabric"))), Util.NIL_UUID);
+			this.minecraft.player.sendSystemMessage(EffectsLeftUpdateHandler.updateInfo.setStyle(EffectsLeftUpdateHandler.updateInfo.getStyle().withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.translatable("el.update.display2"))).withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://curseforge.com/minecraft/mc-mods/effectsleft-fabric"))));
+			this.minecraft.player.sendSystemMessage(EffectsLeftUpdateHandler.updateVersionInfo.setStyle(EffectsLeftUpdateHandler.updateVersionInfo.getStyle().withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.translatable("el.update.display2"))).withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://curseforge.com/minecraft/mc-mods/effectsleft-fabric"))));
 		}
 	}
 
--- src/main/java/net/coolsimulations/EffectsLeft/programmerartinjector/ProgrammerArtResourcePack.java
+++ src/main/java/net/coolsimulations/EffectsLeft/programmerartinjector/ProgrammerArtResourcePack.java
@@ -18,7 +18,6 @@ import com.google.gson.JsonObject;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.TextComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.FilePackResources;
 import net.minecraft.server.packs.FolderPackResources;
@@ -39,131 +38,132 @@ public class ProgrammerArtResourcePack extends FilePackResources {
 
 	private static final Pattern RESOURCE_PACK_PATH = Pattern.compile("[a-z0-9-_]+");
 
-    public ProgrammerArtResourcePack(File file) {
-        super(file);
-    }
+	public ProgrammerArtResourcePack(File file) {
+		super(file);
+	}
 
-    @Override
-    public String getName() {
-        return "Programmer Art";
-    }
+	@Override
+	public String getName() {
+		return "Programmer Art";
+	}
 
-    @Override
-    public boolean hasResource(PackType resourceType, ResourceLocation resource) {
-        boolean contains = super.hasResource(resourceType, resource);
-        if(contains || resourceType.equals(PackType.SERVER_DATA))
-            return contains;
-        String path = String.format("programmer_art/%s/%s/%s", resourceType.getDirectory(), resource.getNamespace(), resource.getPath());
-        try{
-            Enumeration<URL> urls = VanillaPackResources.class.getClassLoader().getResources(path);
-            URL url = null;
+	@Override
+	public boolean hasResource(PackType resourceType, ResourceLocation resource) {
+		boolean contains = super.hasResource(resourceType, resource);
+		if(contains || resourceType.equals(PackType.SERVER_DATA))
+			return contains;
+		String path = String.format("programmer_art/%s/%s/%s", resourceType.getDirectory(), resource.getNamespace(), resource.getPath());
+		try{
+			Enumeration<URL> urls = VanillaPackResources.class.getClassLoader().getResources(path);
+			URL url = null;
 
-            while(urls.hasMoreElements())
-                url = urls.nextElement();
+			while(urls.hasMoreElements())
+				url = urls.nextElement();
 
-            return isValid(path, url);
-        }catch(IOException e){
-            return false;
-        }
-    }
+			return isValid(path, url);
+		}catch(IOException e){
+			return false;
+		}
+	}
 
-    private static boolean isValid(String path, URL url) throws IOException {
-        return url != null && (url.getProtocol().equals("jar") || FolderPackResources.validatePath(new File(url.getFile()), "/"+path));
-    }
+	private static boolean isValid(String path, URL url) throws IOException {
+		return url != null && (url.getProtocol().equals("jar") || FolderPackResources.validatePath(new File(url.getFile()), "/"+path));
+	}
 
-    @Override
-    public InputStream getResource(PackType resourceType, ResourceLocation resource) throws IOException {
-        if(super.hasResource(resourceType, resource))
-            return super.getResource(resourceType, resource);
-        String path = String.format("programmer_art/%s/%s/%s", resourceType.getDirectory(), resource.getNamespace(), resource.getPath());
-        try{
-            Enumeration<URL> urls = VanillaPackResources.class.getClassLoader().getResources(path);
-            URL url = null;
+	@Override
+	public InputStream getResource(PackType resourceType, ResourceLocation resource) throws IOException {
+		if(super.hasResource(resourceType, resource))
+			return super.getResource(resourceType, resource);
+		String path = String.format("programmer_art/%s/%s/%s", resourceType.getDirectory(), resource.getNamespace(), resource.getPath());
+		try{
+			Enumeration<URL> urls = VanillaPackResources.class.getClassLoader().getResources(path);
+			URL url = null;
 
-            while(urls.hasMoreElements()){
-                url = urls.nextElement();
-            }
+			while(urls.hasMoreElements()){
+				url = urls.nextElement();
+			}
 
-            if(isValid(path, url))
-                return url.openStream();
-        }catch(IOException e){
+			if(isValid(path, url))
+				return url.openStream();
+		}catch(IOException e){
 
-        }
-        throw new FileNotFoundException(resource.getPath());
-    }
+		}
+		throw new FileNotFoundException(resource.getPath());
+	}
 
-    @Override
-    public Set<String> getNamespaces(PackType resourceType) {
-        Set<String> namespaces = super.getNamespaces(resourceType);
+	@Override
+	public Set<String> getNamespaces(PackType resourceType) {
+		Set<String> namespaces = super.getNamespaces(resourceType);
 
-        if(!resourceType.equals(PackType.CLIENT_RESOURCES))
-            return namespaces;
+		if(!resourceType.equals(PackType.CLIENT_RESOURCES))
+			return namespaces;
 
-        for(ModContainer container : FabricLoader.getInstance().getAllMods()){
-            Path path = container.getRootPath();
-            path = path.toAbsolutePath().normalize();
-            
-            Path childPath = path.resolve(("programmer_art/"+resourceType.getDirectory()).replace("/", path.getFileSystem().getSeparator())).toAbsolutePath().normalize();
-            if(childPath.startsWith(path) && Files.exists(childPath)){
-                try(DirectoryStream<Path> dirs = Files.newDirectoryStream(childPath, Files::isDirectory)){
-                    for(Path dir : dirs){
-                        String name = dir.getFileName().toString();
-                        name = name.replace(path.getFileSystem().getSeparator(), "");
+		for(ModContainer container : FabricLoader.getInstance().getAllMods()){
+			for(Path path : container.getRootPaths()) {
+				path = path.toAbsolutePath().normalize();
 
-                        if(RESOURCE_PACK_PATH.matcher(name).matches()){
-                            namespaces.add(name);
-                        }else{
-                            System.out.println("Ignoring invalid namespace " + name + " in mod " + container.getMetadata().getId());
-                        }
-                    }
-                }catch(IOException e){
+				Path childPath = path.resolve(("programmer_art/"+resourceType.getDirectory()).replace("/", path.getFileSystem().getSeparator())).toAbsolutePath().normalize();
+				if(childPath.startsWith(path) && Files.exists(childPath)){
+					try(DirectoryStream<Path> dirs = Files.newDirectoryStream(childPath, Files::isDirectory)){
+						for(Path dir : dirs){
+							String name = dir.getFileName().toString();
+							name = name.replace(path.getFileSystem().getSeparator(), "");
 
-                }
-            }
-        }
+							if(RESOURCE_PACK_PATH.matcher(name).matches()){
+								namespaces.add(name);
+							}else{
+								System.out.println("Ignoring invalid namespace " + name + " in mod " + container.getMetadata().getId());
+							}
+						}
+					}catch(IOException e){
 
-        return namespaces;
-    }
+					}
+				}
+			}
+		}
 
-    @Override
-    protected InputStream getResource(String name) throws IOException {
-        if(name.equals("pack.png")){
-        	try {
-        		return VanillaPackResources.class.getClassLoader().getResources("programmer_art/"+name).nextElement().openStream();
-        	} catch (NoSuchElementException e) {
-        		return super.getResource(name);
-        	}
-        }
-        return super.getResource(name);
-    }
+		return namespaces;
+	}
 
-    @SuppressWarnings("unchecked")
-    @Override
-    public <T> T getMetadataSection(MetadataSectionSerializer<T> metaReader) throws IOException {
-        if(metaReader instanceof PackMetadataSectionSerializer)
-            return super.getMetadataSection((MetadataSectionSerializer<T>) READER);
-        return super.getMetadataSection(metaReader);
-    }
+	@Override
+	protected InputStream getResource(String name) throws IOException {
+		if(name.equals("pack.png")){
+			try {
+				return VanillaPackResources.class.getClassLoader().getResources("programmer_art/"+name).nextElement().openStream();
+			} catch (NoSuchElementException e) {
+				return super.getResource(name);
+			}
+		}
+		return super.getResource(name);
+	}
 
-    private static final String DESCRIPTION = "The classic look of Minecraft,\n§8§onow with some mods!";
-    private static final CustomMetadataReader READER = new CustomMetadataReader(DESCRIPTION);
+	@SuppressWarnings("unchecked")
+	@Override
+	public <T> T getMetadataSection(MetadataSectionSerializer<T> metaReader) throws IOException {
+		if(metaReader instanceof PackMetadataSectionSerializer)
+			return super.getMetadataSection((MetadataSectionSerializer<T>) READER);
+		return super.getMetadataSection(metaReader);
+	}
 
-    private static class CustomMetadataReader implements MetadataSectionSerializer<PackMetadataSection>{
+	private static final String DESCRIPTION = "The classic look of Minecraft,\n§8§onow with some mods!";
+	private static final CustomMetadataReader READER = new CustomMetadataReader(DESCRIPTION);
 
-        private final Component text;
+	private static class CustomMetadataReader implements MetadataSectionSerializer<PackMetadataSection>{
 
-        public CustomMetadataReader(String description) {
-            this.text = new TextComponent(description);
-        }
-        
-        public PackMetadataSection fromJson(JsonObject jsonObject) {
-            int pack_format = GsonHelper.getAsInt(jsonObject, "pack_format");
-            return new PackMetadataSection(text, pack_format);
-        }
+		private final Component text;
 
-        public String getMetadataSectionName() {
-            return "pack";
-        }
-    }
+		public CustomMetadataReader(String description) {
+			this.text = Component.literal(description);
+		}
+
+		public PackMetadataSection fromJson(JsonObject jsonObject) {
+			int pack_format = GsonHelper.getAsInt(jsonObject, "pack_format");
+			return new PackMetadataSection(text, pack_format);
+		}
+
+		public String getMetadataSectionName() {
+			return "pack";
+		}
+	}
 
 }
